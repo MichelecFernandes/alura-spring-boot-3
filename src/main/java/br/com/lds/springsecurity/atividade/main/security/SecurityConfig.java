b64a4df6441788dc59fe5d862ed58dcccd910436
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-18 12:06:09 -0300

    feat: gracas a Deus, finalizacao do exercicio.

--- src/main/java/br/com/lds/springsecurity/atividade/main/security/SecurityConfig.java
+++ src/main/java/br/com/lds/springsecurity/atividade/main/security/SecurityConfig.java
@@ -1,4 +1,5 @@
 package br.com.lds.springsecurity.atividade.main.security;
+import br.com.lds.springsecurity.atividade.main.domain.UserModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -27,30 +30,26 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
     @Bean
-    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        auth -> auth.requestMatchers(
+                        auth -> auth
+                                .requestMatchers(
                                         "/listar",
                                         "/swagger-ui.html",
                                         "/swagger-ui/**",
-                                        "/v3/api-docs/**"
+                                        "/v3/api-docs/**",
+                                        "/h2-console/**",
+                                        "/authenticate"
                                 ).permitAll()
-                                .anyRequest().authenticated()
-                )
-                .sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .headers(
-                        headers -> headers.frameOptions(
-                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
-                        )
-                );
+                                .requestMatchers("/api/user/**")
+                                .hasAuthority(UserModel.UserRole.USER.name())
+                                .anyRequest().authenticated()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).headers(
+                        headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)).addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
         return http.build();
     }
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration =  new CorsConfiguration();
